b8dd8ad09a4c89e427db444a296f8e46b5b701eb
Kuzmin Vladimir <Vova-kyzmin@yandex.ru>
2024-12-15 19:35:33 +0000

    Append custom voice for user

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from telethon import TelegramClient, events, functions
-from telethon.tl.types import UpdateGroupCallParticipants, UpdateGroupCall
+from telethon import TelegramClient, events
+from telethon.tl.types import UpdateGroupCallParticipants, UpdateGroupCall, UpdateNewMessage, UpdateShortMessage
 from pytgcalls import PyTgCalls
 from pytgcalls.types import MediaStream
-import os, asyncio
+import os, asyncio, re
+import yt_dlp
 
-def env(key):
-    value = os.environ[key]
-    if value == "":
-        raise Exception("Environ {key} is required")
+def env(key, default=None):
+    value = os.environ.get(key, default=default)
+    if value is None:
+        raise Exception(f"Environ {key} is required")
     return value
 
 client = TelegramClient(
@@ -17,7 +18,7 @@ client = TelegramClient(
     system_version='4.16.30-vxMOGVoice'
 )
 
-voicePath = env("VOICE_PATH")
+voicePath = "data/voice"
 
 voice = PyTgCalls(
     client
@@ -26,8 +27,44 @@ voice.start()
 
 chatIDByCallID = {}
 
+def extract_url(text):
+    url_pattern = re.compile(
+        r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    )
+    match = re.search(url_pattern, text)
+    if match:
+        return match.group(0)
+    return None
+
+def download_voice(url, user_id):
+    downloading = os.path.join(voicePath, f"{user_id}.downloading.mp3")
+    origin = os.path.join(voicePath, f"{user_id}.mp3")
+                       
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': downloading,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'max_filesize': int(env("MAX_VOICE_SIZE", "5")) * 1024 * 1024,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    # Raise max-filesize always return success code
+    if not os.path.exists(downloading):
+        raise Exception("Error while downloading voice")
+
+    if os.path.exists(origin):
+        os.remove(origin)
+
+    os.rename(downloading, origin)
+
 @client.on(events.Raw)
-async def handler(event):
+async def onRaw(event):
     print(event)
     if isinstance(event, UpdateGroupCall):
         chatIDByCallID[event.call.id] = -event.chat_id
@@ -44,18 +81,41 @@ async def handler(event):
 
             chat_id = chatIDByCallID.get(event.call.id, None)
             if chat_id is None:
-                raise Exception("Chat ID not found by call ID {event.call.id}")
+                raise Exception(f"Chat ID not found by call ID {event.call.id}")
             
+            userVoice = os.path.join(voicePath, f"{participant.peer.user_id}.mp3")
+            defaultVoise = os.path.join(voicePath, "default.mp3")
+
+            if not os.path.exists(userVoice):
+                if not os.path.exists(defaultVoise):
+                    raise Exception("Default voice not found")
+                userVoice = defaultVoise
+
             await voice.play(
                 chat_id,
                 MediaStream(
-                    voicePath,
+                    userVoice,
                 ),
             )
 
             await asyncio.sleep(5)
 
             await voice.leave_call(chat_id)
+
+    if isinstance(event, (UpdateShortMessage, UpdateNewMessage)):
+        message = event.message if isinstance(event, UpdateShortMessage) else event.message.message
+        user_id = event.user_id if isinstance(event, UpdateShortMessage) else event.message.peer_id.user_id
+        try:
+            url = extract_url(message)
+            if url is None:
+                raise Exception("URL not found in message")
+            
+            download_voice(url, user_id)
+
+            await client.send_message(user_id, "Voice downloaded")
+        except Exception as e:
+            await client.send_message(user_id, str(e))
+        
 
 with client:
     client.run_until_disconnected()
--- readme.md
+++ readme.md
@@ -2,7 +2,9 @@ Environment variable value
 
 - TG_API_ID - Your application ID
 - TG_API_HASH - Your application Hash [more](https://docs.telethon.dev/en/stable/basic/signing-in.html)
-- VOICE_PATH - Default music where any connect to your chat
+- MAX_VOICE_SIZE - Default: 5 - Max size custom voice in MB
+
+You can set a standard sound by loading it data/voice/default.mp3
 
 The user must be an empty account. It must be added to the chat and a voice call must be started.
 
@@ -12,7 +14,6 @@ The user must be an empty account. It must be added to the chat and a voice call
         --name mog-tg \
         -e TG_API_ID=12345 \
         -e TG_API_HASH=0123456789abcdef0123456789abcdef \
-        -e VOICE_PATH=./some.mp3 \
         -v /opt/mog-tg/data:/data \
         mog-tg
 ```
